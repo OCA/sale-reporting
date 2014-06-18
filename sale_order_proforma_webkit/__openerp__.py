bee64ef7a82b6609d80c2348b6c22a8ba351994e
Vincent Renaville <vincent.renaville@camptocamp.com>
2014-06-18 15:55:09 +0200

    [ADD]  sale_order_proforma_webkit

---
+++ sale_order_proforma_webkit/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2014 Camptocamp SA (http://www.camptocamp.com)
+#     @author Romain Deheele ,Vincent Renaville 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Sales Order ProFormat Report using Webkit Library',
+ 'version': '1.1.1',
+ 'category': 'Reports/Webkit',
+ 'description': """
+Sale order webkit
+#################
+
+* Add a sale proforma report in webkit .
+
+Depends on base_header_webkit community addon available here:
+`https://launchpad.net/webkit-utils <https://launchpad.net/webkit-utils>`_
+    """,
+ 'author': 'Camptocamp',
+ 'website': 'http://www.camptocamp.com',
+ 'depends': ['base', 'report_webkit', 'base_headers_webkit', 'sale'],
+ 'data': ['report.xml',
+          ],
+ 'demo_xml': [],
+ 'test': [],
+ 'installable': True,
+ 'active': False,
+ }
