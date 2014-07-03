320d342ace02db22286c82555c10d31faf15f794
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-07-03 20:05:47 +0200

    fix flakes8

--- sale_order_proforma_webkit/__init__.py
+++ sale_order_proforma_webkit/__init__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #   Copyright (c) 2014 Camptocamp SA (http://www.camptocamp.com)
-#   @author Romain Deheele, Vincent Renaville 
+#   @author Romain Deheele, Vincent Renaville
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,4 +18,3 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
