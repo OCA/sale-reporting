273c4960e86a6206caaf43b5b4be2ec6a2c751a1
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2015-09-08 19:21:12 +0530

    [ADD] Added the sales_report_product_image module

--- sales_report_product_image/models/sale_product.py
+++ sales_report_product_image/models/sale_product.py
@@ -22,21 +22,21 @@
 
 from openerp import models, fields
 
+
 class sale_order(models.Model):
     _inherit = 'sale.order'
 
     print_image = fields.Boolean('Print Image', help="""If ticked, you can see
                     the product image in report of sale order/quotation""")
-    image_sizes = fields.Selection([
-                                    ('image', 'Big sized Image'),
+    image_sizes = fields.Selection([('image', 'Big sized Image'),
                                     ('image_medium', 'Medium Sized Image'),
                                     ('image_small', 'Small Sized Image')],
-                                     'Image Sizes',
-                                   default="image_small",
-                                   help="Image size to be displayed in report")
+                                    'Image Sizes', default="image_small",
+                                    help="Image size to be displayed in report")
+
 
 class sale_order_line(models.Model):
     _inherit = 'sale.order.line'
-    
+
     image_small = fields.Binary('Product Image',
                                 related='product_id.image_small')
