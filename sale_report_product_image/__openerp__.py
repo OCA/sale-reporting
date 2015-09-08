a0bf36e240a8d59bbd30dcf21727880732a75a21
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2015-09-08 19:41:10 +0530

    [ADD] Improved the sale_report_product_image module

--- sale_report_product_image/__openerp__.py
+++ sale_report_product_image/__openerp__.py
@@ -20,24 +20,20 @@
 #
 ##############################################################################
 
-from openerp import models, fields
-
-
-class sale_order(models.Model):
-    _inherit = 'sale.order'
-
-    print_image = fields.Boolean('Print Image', help="""If ticked, you can see
-                    the product image in report of sale order/quotation""")
-    image_sizes = fields.Selection(
-                        [('image', 'Big sized Image'),
-                         ('image_medium', 'Medium Sized Image'),
-                         ('image_small', 'Small Sized Image')], 'Image Sizes',
-                        default="image_small",
-                        help="Image size to be displayed in report")
-
-
-class sale_order_line(models.Model):
-    _inherit = 'sale.order.line'
-
-    image_small = fields.Binary('Product Image',
-                                related='product_id.image_small')
+{
+    'name': 'Product image for sale reports',
+    'author': 'Serpent Consulting Services Pvt. Ltd.,\
+             Odoo Community Association (OCA)',
+    'category': 'Sales Management',
+    'website': 'http://www.serpentcs.com',
+    'version': '8.0.1.0.0',
+    'sequence': 1,
+    'depends': ['sale'],
+    'qweb': [],
+    'data': [
+        'views/sale_product_view.xml',
+        'views/report_saleorder.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
