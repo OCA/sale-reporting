f5b5bdc9213ad831b2c4e5c21daac9928b522eab
Frédéric Siffert <fsiffert@gmail.com>
2017-01-04 00:12:10 +0100

    Update __openerp__.py

--- sale_comment_template/__openerp__.py
+++ sale_comment_template/__openerp__.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 # © 2013-2014 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-{"name": "Sale Comments",
- "summary": "Comments texts templates on Sale documents",
- "version": "9.0.1.0.0",
- "depends": ["sale",
-             "invoice_comment_template",
+{
+ 'name': 'Sale Comments',
+ 'summary': 'Comments texts templates on Sale documents',
+ 'version': '9.0.1.0.0',
+ 'license': 'AGPL-3',
+ 'depends': ['sale',
+             'invoice_comment_template',
              ],
- "author": "Camptocamp,Odoo Community Association (OCA)",
- "data": ["data/sale_order_view.xml",
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'data': ['data/sale_order_view.xml',
           'data/base_comment_template_view.xml',
           'views/report_saleorder.xml',
           'security/ir.model.access.csv',
           ],
- "category": "Sale",
+ 'category': 'Sale',
  'installable': True,}
