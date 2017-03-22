ca4796f8232584c8aafdc8b4b75766f27f6ccdcd
Jordi Ballester <jordi.ballester@eficent.com>
2017-03-22 15:06:17 +0100

    [MIG] sale_proforma_report: Migrated from OdooMRP 8.0 to 9.0

--- sale_proforma_report/__openerp__.py
+++ sale_proforma_report/__openerp__.py
@@ -1,42 +1,24 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2016 OdooMRP Team
+# Copyright 2016 AvanzOSC (<http://www.avanzosc.es>)
+# Copyright 2016 Tecnativa (<http://www.tecnativa.com>)
+# Copyright 2016 Eficent Business and IT Consulting Services, S.L.
+#                (<http://www.eficent.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Sale - Proforma Report",
-    "version": "1.0",
+    "name": "Sale Proforma Report",
+    "version": "9.0.1.0.0",
     "depends": [
         "sale",
     ],
     "author": "OdooMRP team,"
               "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
-    "contributors": [
-        "Juan Ignacio Úbeda <juanignacioubeda@avanzosc.es>",
-    ],
-    "category": "Custom Module",
+              "Tecnativa,"
+              "Eficent",
+    "category": "Sale",
     "website": "http://www.odoomrp.com",
     "summary": "Proforma report option in sale orders",
-    "description": """
-This module creates:
-    * a checkbox 'proforma' on sale.order
-    * Proforma header on sale order report if checkbox is clicked
-
-    """,
     "data": [
         "views/sale_order_view.xml",
         "views/report_saleorder.xml",
