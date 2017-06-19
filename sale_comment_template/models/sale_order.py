02e8db607e4652bbc768005fa34265edb34e7458
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2017-06-19 17:18:20 +0200

    Port module sale_comment_template to V10

---
+++ sale_comment_template/models/sale_order.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Author: Nicolas Bessi
+#    Copyright 2013-2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    """Add text comment"""
+
+    _inherit = "sale.order"
+
+    comment_template1_id = fields.Many2one('base.comment.template',
+                                           string='Top Comment Template')
+    comment_template2_id = fields.Many2one('base.comment.template',
+                                           string='Bottom Comment Template')
+    note1 = fields.Html('Top Comment')
+    note2 = fields.Html('Bottom Comment')
+
+    @api.onchange('comment_template1_id')
+    def _set_note1(self):
+        comment = self.comment_template1_id
+        if comment:
+            self.note1 = comment.get_value(self.partner_id.id)
+
+    @api.onchange('comment_template2_id')
+    def _set_note2(self):
+        comment = self.comment_template2_id
+        if comment:
+            self.note2 = comment.get_value(self.partner_id.id)
+
+    @api.multi
+    def _prepare_invoice(self):
+        values = super(SaleOrder, self)._prepare_invoice()
+        values.update({
+            'comment_template1_id': self.comment_template1_id.id,
+            'comment_template2_id': self.comment_template2_id.id,
+            'note1': self.note1,
+            'note2': self.note2,
+        })
+        return values
