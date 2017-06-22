ecc72dc9e15d3ffdfbd199515d7a09b235b22b22
mreficent <miquel.raich@eficent.com>
2017-06-22 12:05:16 +0200

    [MIG] sale_proforma_report: Migration to 10.0

--- sale_proforma_report/models/sale_order.py
+++ sale_proforma_report/models/sale_order.py
@@ -2,11 +2,11 @@
 # Copyright 2016 OdooMRP Team
 # Copyright 2016 AvanzOSC (<http://www.avanzosc.es>)
 # Copyright 2016 Tecnativa (<http://www.tecnativa.com>)
-# Copyright 2016 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2016-17 Eficent Business and IT Consulting Services, S.L.
 #                (<http://www.eficent.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
