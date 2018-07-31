5592235cca891a4f4ff97f43873c6566bc16920d
xavierjimenez <xavier.jimenez@qubiq.es>
2018-07-31 13:22:02 +0200

    [MIG] sale_comment_template: Migration to 11.0

--- sale_comment_template/__manifest__.py
+++ sale_comment_template/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {"name": "Sale Comments",
  "summary": "Comments texts templates on Sale documents",
- "version": "10.0.1.0.0",
+ "version": "11.0.1.0.0",
  "depends": ["sale",
-             "invoice_comment_template",
+             "account_invoice_comment_template",
              ],
  "author": "Camptocamp,Odoo Community Association (OCA)",
  "license": "AGPL-3",
