31529f2e45a26d555fd70b1ac52a72fd20a6c9e9
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-08-22 16:02:27 +0200

    [FIX] sale_comment_template: Finish migration
    * Fix and simplify tests
    * Adapt view
    * Fix documentation

--- sale_comment_template/__manifest__.py
+++ sale_comment_template/__manifest__.py
@@ -1,19 +1,26 @@
 # Copyright 2013-2014 Nicolas Bessi (Camptocamp SA)
+# Copyright 2018 Qubiq - Xavier Jiménez
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-{"name": "Sale Comments",
- "summary": "Comments texts templates on Sale documents",
- "version": "11.0.1.0.0",
- "depends": ["sale",
-             "account_invoice_comment_template",
-             ],
- "author": "Camptocamp,Odoo Community Association (OCA)",
- "license": "AGPL-3",
- "data": ["views/sale_order_view.xml",
-          'views/base_comment_template_view.xml',
-          'views/report_saleorder.xml',
-          'security/ir.model.access.csv',
-          ],
- "category": "Sale",
- 'installable': True,
- }
+{
+    "name": "Sale Comments",
+    "summary": "Comments texts templates on Sale documents",
+    "version": "11.0.1.0.0",
+    "depends": [
+        "sale",
+        "account_invoice_comment_template",
+    ],
+    "author": "Camptocamp,"
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "data": [
+        "views/sale_order_view.xml",
+        'views/base_comment_template_view.xml',
+        'views/report_saleorder.xml',
+        'security/ir.model.access.csv',
+    ],
+    "category": "Sale",
+    'installable': True,
+}
