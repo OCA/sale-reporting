4b4df2a44a9ef467bfc8c8834420b875f97ec551
cubells <vicent@vcubells.net>
2018-09-27 11:02:57 +0200

    [ADD] sale_order_report_product_image module

---
+++ sale_order_report_product_image/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Order Report Product Image",
+    "summary": "Comments texts templates on Sale documents",
+    "version": "11.0.1.0.0",
+    "author": "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-reporting",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        'views/report_saleorder.xml',
+    ],
+    "category": "Sale",
+    'installable': True,
+}
