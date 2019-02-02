d1d0562218cd45588915020ff00e4e48c5e81849
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-02-02 15:07:32 +0100

    [FIX] sale_comment_template: Fix migration script

--- sale_comment_template/migrations/8.0.1.0.0/post-migration.py
+++ sale_comment_template/migrations/8.0.1.0.0/post-migration.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
 
 
 def migrate_comment_template(cr):
-    cr.execute(
-        """
+    openupgrade.logged_query(
+        cr, """
         UPDATE sale_order so
-        SET comment_template1_id = bct.condition_template_id,
+        SET comment_template1_id = bct.id,
             note1 = bct.text
         FROM base_comment_template bct
         WHERE bct.id = so.condition_template_id
-        AND bct.position = 'before_lines'
-        """)
-    cr.execute(
-        """
+        AND bct.position = 'before_lines'""",
+    )
+    openupgrade.logged_query(
+        cr, """
         UPDATE sale_order so
-        SET comment_template2_id = bct.condition_template_id,
+        SET comment_template2_id = bct.id,
             note2 = bct.text
         FROM base_comment_template bct
         WHERE bct.id = so.condition_template_id
-        AND bct.position = 'after_lines'
-        """)
+        AND bct.position = 'after_lines'""",
+    )
 
 
 @openupgrade.migrate()
