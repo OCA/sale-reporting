a75e0073edf6621633b3f87f4e84e4fce65caa23
ernesto <ernesto.tejeda87@gmail.com>
2019-11-12 10:39:11 -0500

    [FIX] sale_layout_category_hide_detail: Propagate 'Show details' to invoice"
    Propagate values in 'Show details' and 'Show subtotal' fields from Sale Order Lines to Invoice Lines

--- sale_layout_category_hide_detail/models/sale_order.py
+++ sale_layout_category_hide_detail/models/sale_order.py
@@ -13,3 +13,9 @@ class SaleOrderLine(models.Model):
     show_subtotal = fields.Boolean(
         string="Show subtotal",
         default=True)
+
+    def _prepare_invoice_line(self, qty):
+        res = super()._prepare_invoice_line(qty)
+        res.update(show_details=self.show_details,
+                   show_subtotal=self.show_subtotal)
+        return res
