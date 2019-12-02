e00e44913aea6c0a88757dc12f584da34d685729
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-12-02 21:05:19 +0100

    [MIG] sale_report_delivered_subtotal: Migration to v12.0

--- sale_report_delivered_subtotal/__manifest__.py
+++ sale_report_delivered_subtotal/__manifest__.py
@@ -2,14 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Report Delivered subtotal",
-    "version": "11.0.1.0.1",
-    'author': 'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-reporting',
+    "version": "12.0.1.0.0",
+    "author": "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-reporting",
     "category": "Sales",
     "license": "AGPL-3",
     "depends": [
         "sale",
     ],
     "installable": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["sergio-teruel"],
 }
