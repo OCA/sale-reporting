f1e02f8d770721f32ad4d8596aafce25147a6c76
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-12-02 21:05:19 +0100

    [MIG] sale_report_delivered_subtotal: Migration to v12.0

--- sale_report_delivered_subtotal/reports/sale_report.py
+++ sale_report_delivered_subtotal/reports/sale_report.py
@@ -9,15 +9,19 @@ class SaleReport(models.Model):
 
     price_subtotal_delivered = fields.Float(
         string='Total Price Delivered',
-        readonly=True,
     )
 
-    def _select(self):
-        select_str = super(SaleReport, self)._select()
-        select_str += """,
+    def _query(self, with_clause='', fields=None, groupby='', from_clause=''):
+        if fields is None:
+            fields = {}
+        select_str = """ ,
             sum((l.price_subtotal /
                  coalesce(nullif(l.product_uom_qty, 0), 1)
                 ) * l.qty_delivered)
             as price_subtotal_delivered
         """
-        return select_str
+        fields.update({
+            'price_subtotal_delivered': select_str,
+        })
+        return super()._query(with_clause=with_clause, fields=fields,
+                              groupby=groupby, from_clause=from_clause)
