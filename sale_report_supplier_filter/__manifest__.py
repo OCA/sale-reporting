222da988636f8cd5b60ea1a5110934784a7f42a7
Carlos Dauden <carlos.dauden@tecnativa.com>
2020-04-05 16:40:25 +0200

    [MIG] sale_report_supplier_filter: Migration to 12.0

--- sale_report_supplier_filter/__manifest__.py
+++ sale_report_supplier_filter/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Report Filter by Product Suppliers",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/sale-reporting',
