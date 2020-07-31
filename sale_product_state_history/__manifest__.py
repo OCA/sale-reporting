77e5f4c5cc80207b96772a1b130f6e73eedc1d20
Denis Roussel <denis.roussel@acsone.eu>
2020-07-31 12:16:24 +0200

    [10.0][ADD] sale_product_state_history

---
+++ sale_product_state_history/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Product State History',
+    'summary': """
+        Allows to get a report on products per product state on date""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'maintainers': ['rousseldenis'],
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/sale-reporting',
+    'depends': [
+        'product_state_history',
+        'sale',
+        'sales_team',
+    ],
+    'data': [
+        'views/product_state_history.xml',
+    ],
+}
