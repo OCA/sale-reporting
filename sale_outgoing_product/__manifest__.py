2096f361188f4447deeceddef76de1abab4cec8b
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-12-20 09:40:25 -0600

    [ADD] sale_outgoing_products: Add new module to show products to be delivered in sales.

---
+++ sale_outgoing_product/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Outgoing Product In Sales",
+    "summary": "Module that generates a view for outgoing product",
+    "version": "14.0.1.0.0",
+    "category": "Reporting",
+    "author": "Jarsa,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-reporting",
+    "license": "LGPL-3",
+    "depends": ["sale_stock", "sale_management"],
+    "data": ["views/stock_move_views.xml"],
+}
---
+++ sale_outgoing_product/readme/USAGE.rst
@@ -0,0 +1,1 @@
+Go to Sale/ Reporting / Outgoing Products press on the button and the report will be generated.
