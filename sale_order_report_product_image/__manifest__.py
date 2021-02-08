8dffef3b6ec80b0adffcc0d8851317799ce347c2
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2021-02-08 17:42:21 +0100

    [MIG] sale_order_report_product_image: Migration to 14.0

--- sale_order_report_product_image/__manifest__.py
+++ sale_order_report_product_image/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Order Report Product Image",
     "summary": "Show product images on Sale documents",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-reporting",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
