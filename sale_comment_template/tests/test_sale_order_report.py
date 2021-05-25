73443b42fb4c3b4b934a64f10c74cbfa46a133fa
Víctor Martínez <victor.martinez@tecnativa.com>
2021-05-25 17:39:23 +0200

    [IMP] sale_comment_template: Include changes from 13.0

--- sale_comment_template/tests/test_sale_order_report.py
+++ sale_comment_template/tests/test_sale_order_report.py
@@ -1,76 +1,87 @@
 # Copyright 2017 Simone Rubino - Agile Business Group
 # Copyright 2018 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests import tagged
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import Form, TransactionCase
 
 
-@tagged("post_install", "-at_install")
 class TestAccountInvoiceReport(TransactionCase):
-    def setUp(self, *args, **kwargs):
-        super(TestAccountInvoiceReport, self).setUp()
+    def setUp(self):
+        super().setUp()
+        self.company = self.env.ref("base.main_company")
         self.base_comment_model = self.env["base.comment.template"]
-        self.before_comment = self._create_comment("before_lines")
-        self.after_comment = self._create_comment("after_lines")
-        self.partner_id = self.env["res.partner"].create({"name": "Partner Test"})
-        self.sale_order = self.env.ref("sale.sale_order_7")
-        # Trigger qty_to_invoice again
-        for order_line in self.sale_order.order_line:
-            order_line.product_id.invoice_policy = "order"
-        self.sale_order.action_confirm()
-
-        self.sale_order.update(
+        # Create comment related to sale model
+        self.sale_obj = self.env.ref("sale.model_sale_order")
+        self.sale_before_comment = self._create_comment(self.sale_obj, "before_lines")
+        self.sale_after_comment = self._create_comment(self.sale_obj, "after_lines")
+        # Create comment related to move model
+        self.move_obj = self.env.ref("account.model_account_move")
+        self.move_before_comment = self._create_comment(self.move_obj, "before_lines")
+        self.move_after_comment = self._create_comment(self.move_obj, "after_lines")
+        # Create partner
+        self.partner = self.env["res.partner"].create({"name": "Partner Test"})
+        self.partner.base_comment_template_ids = [
+            (4, self.sale_before_comment.id),
+            (4, self.sale_after_comment.id),
+            (4, self.move_before_comment.id),
+            (4, self.move_after_comment.id),
+        ]
+        self.product = self.env["product.product"].create(
             {
-                "comment_template1_id": self.before_comment.id,
-                "comment_template2_id": self.after_comment.id,
+                "name": "Test product",
+                "sale_ok": True,
+                "type": "service",
+                "list_price": 10,
+                "invoice_policy": "order",
             }
         )
-        self.sale_order._set_note1()
-        self.sale_order._set_note2()
+        self.sale_order = self._create_sale_order()
+        self.sale_order.action_confirm()
 
-    def _create_comment(self, position):
+    def _create_sale_order(self):
+        sale_form = Form(self.env["sale.order"])
+        sale_form.partner_id = self.partner
+        with sale_form.order_line.new() as line_form:
+            line_form.product_id = self.product
+        return sale_form.save()
+
+    def _create_comment(self, model, position):
         return self.base_comment_model.create(
             {
                 "name": "Comment " + position,
+                "company_id": self.company.id,
                 "position": position,
                 "text": "Text " + position,
+                "model_ids": [(6, 0, model.ids)],
             }
         )
 
-    def test_comments_in_sale_order(self):
+    def test_comments_in_sale_order_report(self):
         res = (
             self.env["ir.actions.report"]
             ._get_report_from_name("sale.report_saleorder")
             ._render_qweb_html(self.sale_order.ids)
         )
-        self.assertRegex(str(res[0]), self.before_comment.text)
-        self.assertRegex(str(res[0]), self.after_comment.text)
+        self.assertRegexpMatches(str(res[0]), self.sale_before_comment.text)
+        self.assertRegexpMatches(str(res[0]), self.sale_after_comment.text)
 
     def test_comments_in_generated_invoice(self):
-        invoice = self.sale_order._create_invoices()
-        self.assertEqual(
-            invoice.comment_template1_id,
-            self.sale_order.comment_template1_id,
+        invoice = self.sale_order._create_invoices()[0]
+        self.assertTrue(self.move_before_comment in invoice.comment_template_ids)
+        self.assertTrue(self.move_after_comment in invoice.comment_template_ids)
+        self.assertFalse(self.sale_before_comment in invoice.comment_template_ids)
+        self.assertFalse(self.sale_after_comment in invoice.comment_template_ids)
+        res = (
+            self.env["ir.actions.report"]
+            ._get_report_from_name("account.report_invoice")
+            ._render_qweb_html(invoice.ids)
         )
-        self.assertEqual(
-            invoice.comment_template2_id,
-            self.sale_order.comment_template2_id,
-        )
+        self.assertRegexpMatches(str(res[0]), self.move_before_comment.text)
+        self.assertRegexpMatches(str(res[0]), self.move_after_comment.text)
 
-    def test_onchange_partner_id(self):
-        self.partner_id.property_comment_template_id = self.after_comment.id
-        vals = {
-            "partner_id": self.partner_id.id,
-        }
-        new_sale = self.env["sale.order"].new(vals)
-        new_sale.onchange_partner_id_sale_comment()
-        sale_dict = new_sale._convert_to_write(new_sale._cache)
-        new_sale = self.env["sale.order"].create(sale_dict)
-        self.assertEqual(new_sale.comment_template2_id, self.after_comment)
-        self.partner_id.property_comment_template_id = self.before_comment.id
-        new_sale = self.env["sale.order"].new(vals)
-        new_sale.onchange_partner_id_sale_comment()
-        sale_dict = new_sale._convert_to_write(new_sale._cache)
-        new_sale = self.env["sale.order"].create(sale_dict)
-        self.assertEqual(new_sale.comment_template1_id, self.before_comment)
+    def test_comments_in_sale_order(self):
+        self.assertTrue(self.sale_after_comment in self.sale_order.comment_template_ids)
+        self.assertTrue(
+            self.sale_before_comment in self.sale_order.comment_template_ids
+        )
