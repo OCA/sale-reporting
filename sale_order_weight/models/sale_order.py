faf0b5a215b1db3e991f135c9dd937a1d1861bd0
SimoRubi <simone.rubino@agilebg.com>
2021-06-15 10:55:54 +0200

    [IMP] sale_order_weight: black, isort, prettier

--- sale_order_weight/models/sale_order.py
+++ sale_order_weight/models/sale_order.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Andrea Cometa - Apulia Software
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
-from openerp import models, api
+from openerp import api, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.multi
     def total_weight(self):
@@ -17,6 +16,5 @@ class SaleOrder(models.Model):
         total_weight = 0.0
         for line in lines:
             if line.product_id:
-                total_weight += (
-                    line.product_id.weight * line.product_uom_qty)
+                total_weight += line.product_id.weight * line.product_uom_qty
         return total_weight
