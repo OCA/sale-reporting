d878a550b19cc271fa84e1e55b5d47b31c6ca60e
Freni-OSI <fpatel@opensourceintegrators.com>
2021-06-22 11:36:01 +0530

    Travis

--- sale_backorder/__manifest__.py
+++ sale_backorder/__manifest__.py
@@ -2,24 +2,21 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Sale Backorder Report',
-    'version': '12.0.1.0.0',
-    'license': 'LGPL-3',
-    'author': 'Open Source Integrators, Odoo Community Association (OCA)',
-    'summary': 'Report of Un-Invoice Goods Delivered and Backorders',
-    'category': 'Sale',
-    'website': 'https://github.com/OCA/sale-reporting',
-    'depends': [
-        'sale_management',
-        'sale_stock'
+    "name": "Sale Backorder Report",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "summary": "Report of Un-Invoice Goods Delivered and Backorders",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-reporting",
+    "depends": ["sale_management", "sale_stock"],
+    "data": [
+        "views/so_backorder_view.xml",
+        "views/sale_view.xml",
+        "report/so_backorder_report.xml",
+        "wizard/so_backorder_wizard_view.xml",
     ],
-    'data': [
-        'views/so_backorder_view.xml',
-        'views/sale_view.xml',
-        'report/so_backorder_report.xml',
-        'wizard/so_backorder_wizard_view.xml',
-    ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['smangukiya'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["smangukiya"],
 }
