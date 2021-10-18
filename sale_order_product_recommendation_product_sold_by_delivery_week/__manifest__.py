2f0060cc03754bd28e88e63b903def1658a1cf54
david <david.vidal@tecnativa.com>
2021-10-18 16:41:54 +0200

    [ADD] sale_order_product_recommendation_product_sold_by_delivery_week
    TT31742

---
+++ sale_order_product_recommendation_product_sold_by_delivery_week/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product weekly sales hint on sales recommendation wizard",
+    "summary": "Adds the weekly sales field to the recommendation wizard",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Sale",
+    "website": "https://github.com/OCA/sale-reporting",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["product_sold_by_delivery_week", "sale_order_product_recommendation"],
+    "auto_install": True,
+    "data": ["wizard/sale_order_recommendation_view.xml"],
+}
