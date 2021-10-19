9f9d1b243baeef93a69d12eeacc3c0443c30b15e
sergio-teruel <sergio.teruel@tecnativa.com>
2021-10-20 00:01:29 +0200

    [ADD] sale_report_delivered_elaboration: New glue module

---
+++ sale_report_delivered_elaboration/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Report Delivered Elaboration",
+    "version": "13.0.1.0.0",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-reporting",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": ["sale_report_delivered", "sale_elaboration_margin"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["sergio-teruel"],
+    "auto_install": True,
+}
---
+++ sale_report_delivered_elaboration/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Go to *Sales > Reporting > Sale from deliveries* and you'll find the new
+*elaboration cost* measure.
