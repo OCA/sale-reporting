a3e4bfaf05cd8e871e0365e619b3586540a9758c
Carlos Dauden <carlos.dauden@tecnativa.com>
2021-11-17 16:06:55 +0100

    [IMP] product_sold_by_delivery_week: Improve performance pre-searching data instead use dot notation. Add security groups to avoid unnecessary compute. TT32983

--- product_sold_by_delivery_week/__manifest__.py
+++ product_sold_by_delivery_week/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2021 Tecnativa - David Vidal
+# Copyright 2021 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Product weekly sales hint",
     "summary": "Adds a field that graphically hints the weekly product sales",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "development_status": "Beta",
     "category": "Sale",
     "website": "https://github.com/OCA/sale-reporting",
@@ -11,6 +12,11 @@
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
     "depends": ["sale_stock"],
-    "data": ["data/ir_cron.xml", "views/product_views.xml", "views/sale_views.xml"],
+    "data": [
+        "data/ir_cron.xml",
+        "security/product_sold_by_delivery_week_security.xml",
+        "views/product_views.xml",
+        "views/sale_views.xml",
+    ],
     "post_init_hook": "post_init_hook",
 }
