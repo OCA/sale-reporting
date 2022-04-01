5b9a63f88c7ec7e0818874056c9d11165ba72948
angel <angel.garcia@sygel.es>
2022-04-01 14:28:07 +0200

    [14.0][ADD] sale_report_crossed_out_original_price: Sale report crossed out original price when a discount exists

---
+++ sale_report_crossed_out_original_price/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Angel Garcia de la Chica Herrera <angel.garcia@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale Report Crossed Out Original Price",
+    "version": "14.0.1.0.0",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-reporting",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "summary": "Sale report crossed out original price when a discount exists",
+    "depends": [
+        "sale",
+    ],
+    "data": ["reports/report_saleorder_document.xml"],
+    "installable": True,
+}
