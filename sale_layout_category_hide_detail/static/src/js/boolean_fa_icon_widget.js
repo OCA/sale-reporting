25e84f61993ced4aec944416d21e9088617651fe
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-26 09:53:59 +0200

    [FIX] sale_layout_category_hide_detail: Do not allow changes to published invoices or confirmed orders.
    Related to: https://github.com/OCA/sale-reporting/pull/128#discussion_r943373291
    
    TT38579

--- sale_layout_category_hide_detail/static/src/js/boolean_fa_icon_widget.js
+++ sale_layout_category_hide_detail/static/src/js/boolean_fa_icon_widget.js
@@ -1,4 +1,5 @@
 /* Copyright 2019 Tecnativa - Ernesto Tejeda
+ * Copyright 2022 Tecnativa - Víctor Martínez
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 odoo.define("sale_layout_category_hide_detail.boolean_fa_icon_widget", function (
@@ -36,12 +37,25 @@ odoo.define("sale_layout_category_hide_detail.boolean_fa_icon_widget", function 
         // Private
         // --------------------------------------------------------------------------
 
+        _allowEdit: function () {
+            var allow = true;
+            if (
+                (this.model === "account.move.line" &&
+                    this.recordData.parent_state !== "draft") ||
+                (this.model === "sale.order.line" && this.recordData.state !== "draft")
+            ) {
+                allow = false;
+            }
+            return allow;
+        },
+
         /**
          * Render font-awesome icon based on state
          *
          * @override
          * @private
          */
+
         _render: function () {
             // Set icon class
             var fa_icons = this.attrs.options.fa_icons;
@@ -55,9 +69,10 @@ odoo.define("sale_layout_category_hide_detail.boolean_fa_icon_widget", function 
             var hover_false =
                 (terminology && _t(terminology.hover_false)) || _t("Click to check");
             var tip = this.value ? hover_true : hover_false;
+            var style = this._allowEdit() ? "" : "cursor:default";
             // Set template and add it to $el
-            var template = "<span class='fa %s' title='%s' aria-label='%s'></span>";
-            this.$el.empty().append(_.str.sprintf(template, fa_class, tip));
+            var template = "<span class='fa %s' title='%s' style='%s'></span>";
+            this.$el.empty().append(_.str.sprintf(template, fa_class, tip, style));
         },
 
         // --------------------------------------------------------------------------
@@ -71,9 +86,10 @@ odoo.define("sale_layout_category_hide_detail.boolean_fa_icon_widget", function 
          * @param {MouseEvent} event
          */
         _toggleValue: function (event) {
+            this._allowEdit();
             event.preventDefault();
             event.stopPropagation();
-            this._setValue(!this.value);
+            if (this._allowEdit()) this._setValue(!this.value);
         },
     });
 
