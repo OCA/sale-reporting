1e81a22353bfc7de605fe5a4f91faaf20d8229ea
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-21 16:14:24 +0530

    [MIG][15.0]sale_outgoing_product:Migration to 15.0

--- sale_outgoing_product/__manifest__.py
+++ sale_outgoing_product/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Outgoing Product In Sales",
     "summary": "Module that generates a view for outgoing product",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Reporting",
     "author": "Jarsa,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-reporting",
