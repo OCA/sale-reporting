83c6b62691a1a6a07c2066e34d22bc8523f78877
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-04-15 16:04:07 +0200

    [MIG] sale_multicompany_reporting_currency: Migration to 16.0

--- sale_multicompany_reporting_currency/__manifest__.py
+++ sale_multicompany_reporting_currency/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Multicompany Reporting Currency",
     "summary": "Adds Amount in multicompany reporting currency to Sale Order",
-    "version": "15.0.1.0.2",
+    "version": "16.0.1.0.0",
     "category": "Sales",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "license": "AGPL-3",
