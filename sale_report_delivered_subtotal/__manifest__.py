2bfa58d74d843452473b9f0ac495b8ed19c918e5
Kobby Folson <kobbyfolson@gmail.com>
2024-08-01 13:01:27 +0100

    [MIG] sale_report_delivered_subtotal: Migration to 17.0

--- sale_report_delivered_subtotal/__manifest__.py
+++ sale_report_delivered_subtotal/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Report Delivered subtotal",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-reporting",
     "category": "Sales",
