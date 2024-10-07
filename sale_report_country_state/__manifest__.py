0175794f7124d294182094836ad49495ee241484
Carlos Lopez <carlos.lopez@tecnativa.com>
2024-10-07 11:55:59 -0500

    [MIG] sale_report_country_state: Migration to version 16.0

--- sale_report_country_state/__manifest__.py
+++ sale_report_country_state/__manifest__.py
@@ -1,8 +1,9 @@
 # Copyright 2020 Tecnativa - David Vidal
+# Copyright 2024 Tecnativa - Carlos López
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Report Filter by State",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-reporting",
     "category": "Sales",
--- sale_report_country_state/readme/CONTRIBUTORS.rst
+++ sale_report_country_state/readme/CONTRIBUTORS.rst
@@ -2,3 +2,4 @@
 
   * David Vidal
   * João Marques
+  * Carlos López
