4dd3f707ab388afa195bfc9e65576a4a6c9a7cc9
Eduardo De Miguel <edu@moduon.team>
2024-10-23 13:26:55 +0200

    [REF] sale_report_delivered: Refactor some for better inheritance

--- sale_report_delivered/reports/sale_report.py
+++ sale_report_delivered/reports/sale_report.py
@@ -100,6 +100,19 @@ class SaleReportDeliverd(models.Model):
         """
         return select_str
 
+    def _sub_select_signed_qty(self):
+        """Sub select to calculate the cases for the signed quantity"""
+        return """
+        WHEN source_location.usage = 'internal' AND dest_location.usage = 'customer'
+            THEN 1
+        WHEN source_location.usage = 'customer' AND dest_location.usage = 'internal'
+            THEN -1
+        WHEN source_location.usage = 'supplier' AND dest_location.usage = 'customer'
+            THEN 1
+        WHEN source_location.usage = 'customer' AND dest_location.usage = 'supplier'
+            THEN -1
+        """
+
     def _sub_select(self):
         sub_select_str = """
             SELECT
@@ -111,14 +124,7 @@ class SaleReportDeliverd(models.Model):
             CASE
               WHEN dest_location.usage IS NULL
                 THEN 1
-              WHEN source_location.usage = 'internal' AND dest_location.usage = 'customer'
-                THEN 1
-              WHEN source_location.usage = 'customer' AND dest_location.usage = 'internal'
-                THEN -1
-              WHEN source_location.usage = 'supplier' AND dest_location.usage = 'customer'
-                THEN 1
-              WHEN source_location.usage = 'customer' AND dest_location.usage = 'supplier'
-                THEN -1
+              {sub_select_signed_qty}
               ELSE 0
             END AS signed_qty,
             (CASE WHEN t.type IN ('product', 'consu') THEN COALESCE(sm.product_uom_qty, 0.0)
@@ -151,7 +157,9 @@ class SaleReportDeliverd(models.Model):
             sp.id as picking_id,
             sol.purchase_price AS unsigned_purchase_price,
             ROUND(svl.value, cur.decimal_places) AS amount_cost
-        """
+        """.format(
+            sub_select_signed_qty=self._sub_select_signed_qty()
+        )
         return sub_select_str
 
     def _from(self):
@@ -175,8 +183,9 @@ class SaleReportDeliverd(models.Model):
         """
         return from_str
 
-    def _where(self):
-        """Take into account only stock moves from:
+    def _sub_where(self):
+        """
+        Take into account only stock moves from:
 
         Outgoing: Internal to Customer
         Returns: Customer to Internal + to_refund
@@ -184,16 +193,34 @@ class SaleReportDeliverd(models.Model):
         Dropship return: Customer to Supplier
         """
         return """
-            WHERE (sm.state = 'done' OR sm.state IS NULL) AND (
-                (source_location.usage = 'internal' AND dest_location.usage = 'customer') OR
-                (source_location.usage = 'customer' AND dest_location.usage = 'internal'
-                    AND sm.to_refund) OR
-                (source_location.usage = 'supplier' AND dest_location.usage = 'customer'
-                    AND svl.quantity < 0) OR
-                (source_location.usage = 'customer' AND dest_location.usage = 'supplier'
-                    AND svl.quantity > 0)
-            )
+        (
+            source_location.usage = 'internal' AND
+            dest_location.usage = 'customer'
+        ) OR
+        (
+            source_location.usage = 'customer' AND
+            dest_location.usage = 'internal' AND
+            sm.to_refund
+        ) OR
+        (
+            source_location.usage = 'supplier' AND
+            dest_location.usage = 'customer' AND
+            svl.quantity < 0
+        ) OR
+        (
+            source_location.usage = 'customer' AND
+            dest_location.usage = 'supplier' AND
+            svl.quantity > 0
+        )
         """
+
+    def _where(self):
+        """Where clause with only done mvoes or without state"""
+        return """
+            WHERE (sm.state = 'done' OR sm.state IS NULL) AND ({sub_where})
+        """.format(
+            sub_where=self._sub_where()
+        )
 
     def _group_by(self):
         group_by_str = """
