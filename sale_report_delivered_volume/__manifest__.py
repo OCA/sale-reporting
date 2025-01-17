bac47471d657da3bac86d1482efa0963945419f5
Bhavesh Heliconia <oca@heliconia.in>
2025-01-17 17:46:00 +0530

    [MIG] sale_report_delivered_volume: Migration to 17.0

--- sale_report_delivered_volume/__manifest__.py
+++ sale_report_delivered_volume/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Sale Report Delivered Volume",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Sales",
     "development_status": "Production/Stable",
