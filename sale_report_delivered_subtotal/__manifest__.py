baa65ec68fb178332913cbe48c51e40eebe619e1
oca-ci <oca-ci@odoo-community.org>
2025-06-26 17:53:32 +0000

    [UPD] Update sale_report_delivered_subtotal.pot

--- sale_report_delivered_subtotal/__manifest__.py
+++ sale_report_delivered_subtotal/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Report Delivered subtotal",
     "version": "18.0.1.0.0",
-    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "Tecnativa,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-reporting",
     "category": "Sales",
     "license": "AGPL-3",
