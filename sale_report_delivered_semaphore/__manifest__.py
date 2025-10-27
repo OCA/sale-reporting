98d43a2e8f53280e90b51411086edb99a614e9cf
CarlosRoca13 <carlos.roca@tecnativa.com>
2025-10-27 11:42:56 +0100

    [ADD] sale_report_delivered_semaphore: New module group by semaphore on sale report delivered

---
+++ sale_report_delivered_semaphore/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Report Delivered Semaphore",
+    "version": "15.0.1.0.0",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-reporting",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": ["sale_report_delivered", "sale_semaphore"],
+    "data": ["views/sale_report_delivered_views.xml"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["CarlosRoca13"],
+}
